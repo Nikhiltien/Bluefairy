0809c7b41cd8492994b26e74fc9a38c90f5b72b9
nikhiltien <nikhil17tn@gmail.com>
2023-10-30 19:39:39 -0400

    board extraction

--- src/engine/evaluator.rs
+++ src/engine/evaluator.rs
@@ -29,4 +29,76 @@ impl BoardEvaluator {
         // Placeholder code for feature extraction
         vec![]
     }
+    
+    fn calculate_material_count(&self, board_state: &BoardState) -> f64 {
+        let mut material_white = 0.0;
+        let mut material_black = 0.0;
+        
+        for square in chess::ALL_SQUARES.iter() {
+            match board_state.board.piece_on(*square) {
+                Some(piece) => {
+                    let value = match piece {
+                        chess::Piece::Pawn => 1.0,
+                        chess::Piece::Knight => 3.0,
+                        chess::Piece::Bishop => 3.0,
+                        chess::Piece::Rook => 5.0,
+                        chess::Piece::Queen => 9.0,
+                        chess::Piece::King => 0.0, // Kings are generally not included in material count
+                    };
+                    if board_state.board.color_on(*square) == Some(chess::Color::White) {
+                        material_white += value;
+                    } else {
+                        material_black += value;
+                    }
+                },
+                None => continue,
+            }
+        }
+        material_white - material_black
+    }
+
+    const PAWN_TABLE: [f64; 64] = [
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0,
+        1.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0, 1.0,
+        0.5, 0.5, 1.0, 2.5, 2.5, 1.0, 0.5, 0.5,
+        0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0,
+        0.5, -0.5,-1.0, 0.0, 0.0,-1.0,-0.5, 0.5,
+        0.5, 1.0, 1.0,-2.0,-2.0, 1.0, 1.0, 0.5,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+    ];
+    const KNIGHT_TABLE: [f64; 64] = [];
+    const BISHOP_TABLE: [f64; 64] = [];
+    const ROOK_TABLE: [f64; 64] = [];
+    const QUEEN_TABLE: [f64; 64] = [];
+    const KING_TABLE: [f64; 64] = [];
+
+    /// Calculate the Piece-Square Table score based on the placement of pieces.
+    fn calculate_pst_score(&self, board_state: &BoardState) -> f64 {
+        let mut pst_white = 0.0;
+        let mut pst_black = 0.0;
+
+        for square in chess::ALL_SQUARES.iter() {
+            match board_state.board.piece_on(*square) {
+                Some(piece) => {
+                    let square_idx = square.to_index();
+                    let value = match piece {
+                        chess::Piece::Pawn => Self::PAWN_TABLE[square_idx],
+                        chess::Piece::Knight => Self::KNIGHT_TABLE[square_idx],
+                        chess::Piece::Bishop => Self::BISHOP_TABLE[square_idx],
+                        chess::Piece::Rook => Self::ROOK_TABLE[square_idx],
+                        chess::Piece::Queen => Self::QUEEN_TABLE[square_idx],
+                        chess::Piece::King => Self::KING_TABLE[square_idx],
+                    };
+                    if board_state.board.color_on(*square) == Some(chess::Color::White) {
+                        pst_white += value;
+                    } else {
+                        pst_black += value;
+                    }
+                },
+                None => continue,
+            }
+        }
+        pst_white - pst_black
+    }
 }
--- src/utilities.rs
+++ src/utilities.rs
@@ -1,8 +1,29 @@
+extern crate chess;
+
 pub fn convert_move_to_string(chess_move: chess::ChessMove) -> String {
-    // Implement logic to convert a ChessMove object to its string representation
+    let from = chess_move.get_source().to_string();
+    let to = chess_move.get_dest().to_string();
+    let promotion = chess_move.get_promotion().map_or(String::new(), |p| p.to_string());
+    
+    format!("{}{}{}", from, to, promotion)
 }
 
 pub fn convert_string_to_move(move_str: &str) -> Result<chess::ChessMove, String> {
-    // Implement logic to convert a string to a ChessMove object
-    // Return an error message if the conversion fails
+    if move_str.len() < 4 {
+        return Err("Invalid move string length".to_string());
+    }
+
+    let from = &move_str[0..2];
+    let to = &move_str[2..4];
+    let promotion = if move_str.len() > 4 { Some(&move_str[4..]) } else { None };
+
+    let from_square = chess::Square::from_str(from).map_err(|_| "Invalid source square".to_string())?;
+    let to_square = chess::Square::from_str(to).map_err(|_| "Invalid destination square".to_string())?;
+    let promotion_piece = promotion.map(|p| chess::Piece::from_str(p).map_err(|_| "Invalid promotion piece".to_string()));
+
+    if let Some(Err(err)) = promotion_piece {
+        return Err(err);
+    }
+
+    Ok(chess::ChessMove::new(from_square, to_square, promotion_piece.ok().flatten()))
 }
