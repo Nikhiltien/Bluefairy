ab2dd2f16d15f1dca70b76e3f16175d9ecd11a37
nikhiltien <nikhil17tn@gmail.com>
2023-11-26 23:00:23 -0500

    EvaluationBar bugfix

--- gui/bluefairy/src/components/EvaluationBar.js
+++ gui/bluefairy/src/components/EvaluationBar.js
@@ -3,32 +3,42 @@ import { Paper, Typography, Box } from '@mui/material';
 
 const EvaluationBar = ({ score }) => {
     const [progressHeight, setProgressHeight] = useState('50%');
+    const [barPosition, setBarPosition] = useState('bottom');
 
     useEffect(() => {
-        const normalizedHeight = ((score + 10) / 20) * 100 + '%';
-        setProgressHeight(normalizedHeight);
+        let normalizedHeight;
+        if (score >= 0) {
+            // Positive scores fill from top
+            normalizedHeight = 50 + ((score / 10) * 50);
+            setBarPosition('top');
+        } else {
+            // Negative scores fill from bottom
+            normalizedHeight = 50 - ((Math.abs(score) / 10) * 50);
+            setBarPosition('bottom');
+        }
+        setProgressHeight(`${Math.max(0, Math.min(100, normalizedHeight))}%`);
     }, [score]);
 
-    const barColor = score > 0 ? '#A9A9A9' : '#D3D3D3';  // Dark grey for positive, light grey for negative
+    // Swap colors here
+    const barColor = score > 0 ? '#D3D3D3' : '#A9A9A9';  // Light grey for positive, dark grey for negative
 
-    const displayScore = score !== null ? score.toFixed(2).toString() : '0';  // Display score with 2 decimal places
+    const displayScore = score !== null ? score.toFixed(2).toString() : '0';
 
     return (
-        <Paper style={{ width: '40px', height: '550px', marginLeft: '20px', textAlign: 'center', backgroundColor: '#333333', position: 'relative' }}>  {/* Dark grey background */}
+        <Paper style={{ width: '40px', height: '550px', marginLeft: '20px', textAlign: 'center', backgroundColor: '#333333', position: 'relative' }}>
             <Box
                 sx={{
                     position: 'absolute',
-                    bottom: '5px', // Reduced padding
+                    bottom: 0,
                     left: '5px',
                     right: '5px',
-                    top: '5px',
                     height: progressHeight,
                     backgroundColor: barColor,
                     border: '1px solid #888',
-                    transition: 'height 0.5s ease-in-out',  // Animate height change
+                    transition: 'height 0.5s ease-in-out',
                 }}
             />
-            <Typography variant="body1" style={{ position: 'absolute', width: '100%', top: '50%', transform: 'translateY(-50%)', color: '#FFFFFF' }}>  {/* White text for contrast */}
+            <Typography variant="body1" style={{ position: 'absolute', width: '100%', top: '50%', transform: 'translateY(-50%)', color: '#FFFFFF' }}>
                 {displayScore}
             </Typography>
         </Paper>
