4e9a78fc8b729d15b7b5273969d0af6a236428d8
nikhiltien <nikhil17tn@gmail.com>
2023-11-27 01:33:51 -0500

    profile card updates

--- gui/bluefairy/src/components/ProfileCard.js
+++ gui/bluefairy/src/components/ProfileCard.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, Typography, Avatar, Grid, CardActions, Button } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person'; // You can replace this with an actual image
 
 const ProfileCard = ({ name, elo, winRate }) => {
   return (
-    <Card style={{ marginBottom: 20 }}>
+    <Card style={{ marginBottom: 20, maxWidth: 345 }}>
       <CardContent>
-        <Typography variant="h5" component="div">
-          {name}
-        </Typography>
-        <Typography color="text.secondary">
-          Elo: {elo}
-        </Typography>
-        <Typography color="text.secondary">
-          Win Rate: {winRate}
-        </Typography>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item>
+            <Avatar>
+              <PersonIcon />
+            </Avatar>
+          </Grid>
+          <Grid item xs>
+            <Typography gutterBottom variant="h5" component="div">
+              {name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Elo: {elo}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Win Rate: {winRate}
+            </Typography>
+          </Grid>
+        </Grid>
       </CardContent>
+      <CardActions>
+        <Button size="small">View Profile</Button>
+        {/* <Button size="small">Challenge</Button> */}
+      </CardActions>
     </Card>
   );
 };
--- gui/bluefairy/src/components/ProfilesPage.js
+++ gui/bluefairy/src/components/ProfilesPage.js
@@ -1,58 +1,102 @@
 import React, { useState } from 'react';
-import ProfileCard from './ProfileCard'; // Assuming you have a ProfileCard component
+import ProfileCard from './ProfileCard';
+import { TextField, Grid, Container, Typography, Slider, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 const ProfilesPage = () => {
-  // Dummy profile data for testing
+  // Dummy data
   const dummyProfiles = [
-    { name: 'John Doe', elo: 1500, winRate: '70%' },
-    { name: 'Alice Smith', elo: 1600, winRate: '65%' },
-    { name: 'Bob Johnson', elo: 1450, winRate: '75%' },
+    { name: 'John Doe', elo: 1500, winRate: '70%', opening: 'Sicilian Defense' },
+    { name: 'Alice Smith', elo: 1600, winRate: '65%', opening: 'French Defense' },
+    { name: 'Bob Johnson', elo: 1450, winRate: '75%', opening: 'Ruy Lopez' },
     // Add more profiles as needed
   ];
 
-  // State for the search input
+  // State for filters
   const [searchInput, setSearchInput] = useState('');
+  const [eloRange, setEloRange] = useState([1000, 2000]);
+  const [selectedOpening, setSelectedOpening] = useState('');
 
-  // State for filtered profiles
-  const [filteredProfiles, setFilteredProfiles] = useState(dummyProfiles);
+  // Filter profiles
+  const filteredProfiles = dummyProfiles.filter((profile) =>
+    profile.name.toLowerCase().includes(searchInput.toLowerCase()) &&
+    profile.elo >= eloRange[0] && profile.elo <= eloRange[1] &&
+    (selectedOpening === '' || profile.opening === selectedOpening)
+  );
 
-  // Handle search input change
+  // Handlers
   const handleSearchInputChange = (e) => {
-    const inputValue = e.target.value;
-    setSearchInput(inputValue);
-
-    // Filter profiles based on the search input
-    const filtered = dummyProfiles.filter((profile) =>
-      profile.name.toLowerCase().includes(inputValue.toLowerCase())
-    );
-
-    setFilteredProfiles(filtered);
+    setSearchInput(e.target.value);
   };
 
+  const handleEloRangeChange = (event, newValue) => {
+    setEloRange(newValue);
+  };
+
+  const handleOpeningChange = (event) => {
+    setSelectedOpening(event.target.value);
+  };
+
+  // Unique list of openings for the dropdown
+  const openings = Array.from(new Set(dummyProfiles.map(p => p.opening)));
+
   return (
-    <div>
-      <h1>Profiles</h1>
-      <div>
-        {/* Search bar */}
-        <input
-          type="text"
-          placeholder="Search profiles..."
-          value={searchInput}
-          onChange={handleSearchInputChange}
-        />
-      </div>
-      <div>
-        {/* Profile cards */}
+    <Container maxWidth="md" style={{ marginTop: '20px' }}>
+      <Typography variant="h4" gutterBottom style={{ color: 'lightgrey' }}>
+        Profiles
+      </Typography>
+      
+      {/* Search Bar */}
+      <TextField
+        fullWidth
+        label="Search profiles..."
+        variant="outlined"
+        value={searchInput}
+        onChange={handleSearchInputChange}
+        style={{ marginBottom: '20px', backgroundColor: 'lightgrey', borderRadius: 4, color: 'black' }}
+      />
+
+      {/* Elo Range Slider */}
+      <Typography gutterBottom>Elo Range</Typography>
+      <Slider
+        value={eloRange}
+        onChange={handleEloRangeChange}
+        valueLabelDisplay="auto"
+        min={1000}
+        max={3000}
+        style={{ marginBottom: '20px' }}
+      />
+
+      {/* Opening Dropdown */}
+      <FormControl fullWidth style={{ marginBottom: '20px' }}>
+        <InputLabel>Most Played Opening</InputLabel>
+        <Select
+          value={selectedOpening}
+          label="Most Played Opening"
+          onChange={handleOpeningChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {openings.map((opening, index) => (
+            <MenuItem key={index} value={opening}>{opening}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+
+      {/* Profiles Grid */}
+      <Grid container spacing={3}>
         {filteredProfiles.map((profile, index) => (
-          <ProfileCard
-            key={index}
-            name={profile.name}
-            elo={profile.elo}
-            winRate={profile.winRate}
-          />
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <ProfileCard
+              name={profile.name}
+              elo={profile.elo}
+              winRate={profile.winRate}
+              opening={profile.opening}
+            />
+          </Grid>
         ))}
-      </div>
-    </div>
+      </Grid>
+    </Container>
   );
 };
 
